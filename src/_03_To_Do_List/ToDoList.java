6ef077e4a52926d85089053c7391463b728f1c71
cj3an <cj3an@DESKTOP-3LMP021.home>
2021-09-20 18:31:59 -0700

    ToDoList.java

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,10 +1,15 @@
 package _03_To_Do_List;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ToDoList {
+public class ToDoList implements ActionListener{
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	 *
@@ -26,6 +31,8 @@ public class ToDoList {
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
 
+	ArrayList<String> tasks = new ArrayList<>();
+	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton("Add task");
@@ -33,13 +40,63 @@ public class ToDoList {
 	JButton button3 = new JButton("Remove task");
 	JButton button4 = new JButton("Save list");
 	JButton button5 = new JButton("Load list");
-
+	
+	
+	
+	ToDoList(){	
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	panel.add(button3);
+	panel.add(button4);
+	panel.add(button5);
+	
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	button3.addActionListener(this);
+	button4.addActionListener(this);
+	button5.addActionListener(this);
+	
+	frame.setVisible(true);
+	frame.pack();
+	}
+	
 	public static void main(String[] args) {
-		
+	ToDoList tdl = new ToDoList();
 	
 	
 	
-	
-	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getSource() == button1){
+			String response = JOptionPane.showInputDialog("Enter a task");
+			tasks.add(response);
+		}
+		if(arg0.getSource() == button2){
+			String allTasks= "";
+			for(int i = 0 ; i<tasks.size(); i++){
+				allTasks += i+1 + ") " + tasks.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, allTasks);
+		}
+		if(arg0.getSource() == button3){
+			String res = JOptionPane.showInputDialog("Which task do you want to remove?");
+			//int num = Integer.parseInt(res);
+			
+			for(int i = 0; i < tasks.size(); i++){
+				if(tasks.get(i).equals(res)){
+					tasks.remove(i);
+				}
+			}
+		}
+		if(arg0.getSource() == button4){
+			//copy all elements in arraylist and write them into file
+		}
+		if(arg0.getSource() == button5){
+			//use jfilechooser to choose and intialize new arraylist member variable
+	}
 	}
 }
