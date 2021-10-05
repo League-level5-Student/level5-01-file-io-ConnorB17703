ce4e651ce42a87ba0b625f31ac31cb935b4d913a
cj3an <cj3an@DESKTOP-3LMP021.home>
2021-10-04 18:23:14 -0700

    DirectoryIterator.java

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,6 +1,10 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 import javax.swing.JFileChooser;
 
@@ -10,19 +14,19 @@ public class DirectoryIterator {
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
-		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		int returnVal = jfc.showOpenDialog(null);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
-				}
-			}
-		}
-		
+//		JFileChooser jfc = new JFileChooser();
+//		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+//		int returnVal = jfc.showOpenDialog(null);
+//		if (returnVal == JFileChooser.APPROVE_OPTION) {
+//			File directory = jfc.getSelectedFile();
+//			File[] files = directory.listFiles();
+//			if(files != null) {
+//				for(File f : files) {
+//				  System.out.println(f.getAbsolutePath());
+//				}
+//			}
+//		}
+//		
 		/*
 		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
 		 * For every .java file it finds, the program will add a (non-legally binding) copyright statement at the bottom.
@@ -31,12 +35,41 @@ public class DirectoryIterator {
 		 */
 		
 		JFileChooser jfc2 = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jfc2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		int returnVal2 = jfc2.showOpenDialog(null);
 		
+//		File[] jProjects;
 		
-		//***YOU LEFT OFF HERE***
-		//itrate through src folder and check if files with .java to add statment
+		if(returnVal2 == JFileChooser.APPROVE_OPTION){
+			File src = jfc2.getSelectedFile();
+			File[] directories = src.listFiles();
+			if(directories != null){
+				for(File d : directories){
+					System.out.println("found a directory --> " + d.getAbsolutePath());
+					File[] jProjects = d.listFiles();
+					for(File jP : jProjects){
+						String ext = jP.getAbsolutePath().substring(jP.getAbsolutePath().length() - 5);
+						//System.out.println("\t " + ext);
+						if(ext.contains(".java")){
+							System.out.println("\n java project");
+							try{
+								FileWriter fw = new FileWriter(jP.getAbsolutePath(), true);
+								
+								//****FINISH WRITING CODE TO WRITE STUFF IN THE FILES****
+								
+							}catch(IOException e){
+								e.printStackTrace();
+							}
+							
+						}
+						
+					}
+				}
+			}else{
+				System.out.println("no files found");
+			}
+			
+		}
 		
 		
 	}
